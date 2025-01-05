90225b83a1ed9a33b8da45c6975d9d268da3f541
Julia <juliac.3195@gmail.com>
2025-01-05 14:16:27 +0100

    nginx

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-if [ ! -f "/var/www/inception/wp-config.php" ]; then
-   mv /tmp/wp-config.php /var/www/inception/
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+   mv /tmp/wp-config.php /var/www/html/
 fi
 
 sleep 10
 
-wp --allow-root --path="/var/www/inception/" core download || true
+wp --allow-root --path="/var/www/html/" core download || true
 
-if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+if ! wp --allow-root --path="/var/www/html/" core is-installed;
 then
-    wp  --allow-root --path="/var/www/inception/" core install \
+    wp  --allow-root --path="/var/www/html/" core install \
         --url=$WP_URL \
         --title=$WP_TITLE \
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL
     
-    wp  --allow-root --path="/var/www/inception/" user create \
+    wp  --allow-root --path="/var/www/html/" user create \
         $WP_USER \
         $WP_EMAIL \
         --user_pass=$WP_PASSWORD \
         --role=editor
 fi;
 
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+wp --allow-root --path="/var/www/html/" theme install raft --activate 
 
 exec $@
